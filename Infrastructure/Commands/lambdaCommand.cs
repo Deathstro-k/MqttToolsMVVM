6198d3571d1eefc9cb16f1235824f1dacfd069f0
Trofimov Alexandr <sanechichek.trofimov@gmail.com>
2022-09-16 21:18:01 +0300

    Создана LambdaCommand

---
+++ Infrastructure/Commands/lambdaCommand.cs
@@ -0,0 +1,23 @@
+﻿using MqttToolsMVVM.Infrastructure.Commands.Base;
+using System;
+
+namespace MqttToolsMVVM.Infrastructure.Commands
+{
+    internal class lambdaCommand : Command
+    {
+
+        private Action<object> _Execute;
+
+        private Func<object, bool> _CanExecute;
+        public void  LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null) 
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+       
+
+        public override void Execute(object parameter) => _Execute(parameter); 
+       
+    }
+}
