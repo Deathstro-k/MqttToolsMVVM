ffa6ba78842bc9d96d8c7da802e345dc92eaacef
Trofimov Alexandr <sanechichek.trofimov@gmail.com>
2022-09-16 20:39:10 +0300

    Создан базовый класс команды

---
+++ Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MqttToolsMVVM.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested+=value;
+            remove => CommandManager.RequerySuggested -= value;                    
+        }
+
+        /// <summary>
+        /// Проверяет, возмодно ли сделать эту команду
+        /// </summary>
+      
+        public abstract bool CanExecute(object parameter);
+
+
+        /// <summary>
+        /// Основная логика команды
+        /// </summary>        
+
+        public abstract void Execute(object parameter);
+      
+    }
+}
